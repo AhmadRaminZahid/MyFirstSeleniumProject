e8d7e29e2b0b3958e3d0c6ca275ebacc54183bb0
AhmadRaminZahid <ahmadraminzahid1120@gmail.com>
2024-01-06 17:47:32 +0430

    hw

--- src/test/java/Practice/Q4Calculate.java
+++ src/test/java/Practice/Q4Calculate.java
@@ -6,7 +6,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 import java.time.Duration;
 
@@ -54,14 +56,75 @@ public class Q4Calculate {
     //     HW
     @Test
     public void multiplyTest(){
+        //        Navigate to  https://testpages.herokuapp.com/styled/index.html
+        driver.get("https://testpages.herokuapp.com/styled/index.html");
+//        Click on  Calculator under Micro Apps
+        driver.findElement(By.id("calculatetest")).click();
+//        Type any number in the first input
+        driver.findElement(By.id("number1")).sendKeys("6");
+//        Type any number in the second input
+        driver.findElement(By.cssSelector("#number2")).sendKeys("8");
+       WebElement dropdown =driver.findElement(By.xpath("//select[@id='function']"));
+        Select multiply= new Select(dropdown);
+        multiply.selectByValue("times");
+//        Click on Calculate
+        driver.findElement(By.id("calculate")).click();
+//        Get the result
+        String result = driver.findElement(By.id("answer")).getText();
+
+//        Verify the result
+        Assertions.assertEquals(result, "48");
+//        Print the result
+        System.out.println("result = " + result);
+//        Close the browser by using @AfterEach annotation
 
     }
     @Test
     public void subtractTest(){
+//        Navigate to  https://testpages.herokuapp.com/styled/index.html
+        driver.get("https://testpages.herokuapp.com/styled/index.html");
+//        Click on  Calculator under Micro Apps
+        driver.findElement(By.id("calculatetest")).click();
+//        Type any number in the first input
+        driver.findElement(By.id("number1")).sendKeys("6");
+//        Type any number in the second input
+        driver.findElement(By.cssSelector("#number2")).sendKeys("8");
+        WebElement dropdown =driver.findElement(By.xpath("//select[@id='function']"));
+        Select subtract= new Select(dropdown);
+        subtract.selectByValue("minus");
+//        Click on Calculate
+        driver.findElement(By.id("calculate")).click();
+//        Get the result
+        String result = driver.findElement(By.id("answer")).getText();
 
+//        Verify the result
+        Assertions.assertEquals(result, "-2");
+//        Print the result
+        System.out.println("result = " + result);
+//        Close the browser by using @AfterEach annotation
     }
     @Test
     public void divisionTest(){
+//        Navigate to  https://testpages.herokuapp.com/styled/index.html
+        driver.get("https://testpages.herokuapp.com/styled/index.html");
+//        Click on  Calculator under Micro Apps
+        driver.findElement(By.id("calculatetest")).click();
+//        Type any number in the first input
+        driver.findElement(By.id("number1")).sendKeys("6");
+//        Type any number in the second input
+        driver.findElement(By.cssSelector("#number2")).sendKeys("8");
+        WebElement dropdown =driver.findElement(By.xpath("//select[@id='function']"));
+        Select divide= new Select(dropdown);
+        divide.selectByValue("divide");
+//        Click on Calculate
+        driver.findElement(By.id("calculate")).click();
+//        Get the result
+        String result = driver.findElement(By.id("answer")).getText();
 
+//        Verify the result
+        Assertions.assertEquals(result, "0.75");
+//        Print the result
+        System.out.println("result = " + result);
+//        Close the browser by using @AfterEach annotation
     }
 }
